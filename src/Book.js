25cd41564b17cb8189f277ef93dc69be0ca99949
Aritz Beraza <aberazag@cisco.com>
2017-10-30 17:29:21 +0100

    feat: Dropdown focuses on the actual state of book

--- src/Book.js
+++ src/Book.js
@@ -1,30 +1,26 @@
 import React from 'react'
 
 class Book extends React.Component {
-    state = {
-        title:"",
-        authors: "",
-        coverURL: "",
-        status : "none"
-    }
-
+    
     render() {
+        const {title, authors, cover, status} = this.props.data;
+
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.data.cover }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: cover }}></div>
                             <div className="book-shelf-changer">
-                              <select>
+                              <select value={status}>
                                 <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
+                                <option value="reading">Currently Reading</option>
+                                <option value="wishlist">Want to Read</option>
                                 <option value="read">Read</option>
                                 <option value="none">None</option>
                               </select>
                             </div>
                           </div>
-                          <div className="book-title">this.props.data.title</div>
-                          <div className="book-authors">this.props.data.authors</div>
+                          <div className="book-title">{title}</div>
+                          <div className="book-authors">{authors}</div>
                         </div>
 
         )
