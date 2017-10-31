090e0c80a423fe8da73ac4aab4ac13c52b7ee64e
Aritz Beraza <aberazag@cisco.com>
2017-10-31 10:54:11 +0100

    feat: Partial book search implementation
    Book search works and retrieves data. Book model to be used not yet defined so some glitches like no covers or wrong display of multiple authors are present.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
+
 class SearchBooks extends Component {
-    
+    state = {
+        query : '',
+        results : []
+    }
+
+    searchTimer = NaN;
+
+    updateQuery = query => {
+        console.log( `updateQuery(${query})` );
+        (this.searchTimer && clearTimeout(this.searchTimer))
+
+        if(query === ''){
+            this.setState({query:'', results:[]});
+            return;
+        }
+
+        this.searchTimer = setTimeout(
+            _=> BooksAPI.search(query.trim(), 20)
+            .then((results) => {console.dir(results); return results;})
+            .then((results)=>this.setState({query:query.trim(), results}))
+            , 150);
+    } 
+
+    clearQuery = _ => this.setState({query:'', results:[]})
+
     render(){
         return (
         <div className="search-books">
@@ -18,12 +45,16 @@ class SearchBooks extends Component {
             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
             you don't find a specific author or title. Every search is limited by search terms.
         */}
-                    <input type="text" placeholder="Search by title or author"/>
+                    <input type="text" 
+                        placeholder="Search by title or author"
+                        onChange={e => this.updateQuery(e.target.value)}/>
         
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    { this.state.results.map( b => (<li key={b.authors+b.title}><Book data={b} /></li>)) }
+                </ol>
             </div>
         </div>
         )
